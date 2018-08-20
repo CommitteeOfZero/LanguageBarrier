46a21bb2eb81526bd66df0dc5e6c1196dd43b2bf
Niklas K <drdaxxy@googlemail.com>
2018-08-21 01:13:39 +0200

    Crash reporting: custom icon

--- LanguageBarrier/ErrorHandling.cpp
+++ LanguageBarrier/ErrorHandling.cpp
@@ -43,6 +43,7 @@ void errorHandlingInit() {
   info.pszAppName = appName.c_str();
   info.pszAppVersion = appVersion.c_str();
   info.pszUrl = url.c_str();
+  info.pszCustomSenderIcon = L"languagebarrier\\crash.ico,0";
   info.uPriorities[CR_HTTP] = 1;
   info.uPriorities[CR_SMTP] = CR_NEGATIVE_PRIORITY;
   info.uPriorities[CR_SMAPI] = CR_NEGATIVE_PRIORITY;
